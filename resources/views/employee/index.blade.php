8cbf0c9677650ce3a62c8b3c9f55e471ad846fb4
ranggarh <ranggarhoke@gmail.com>
2023-07-12 11:56:44 +0700

    Laravel 3rd Done

---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $pageTitle }}</title>
+    @vite('resources/sass/app.scss')
+</head>
+<body>
+
+    @extends('layouts.app')
+
+    @section('content')
+
+    <div class="container mt-4">
+        <div class="row mb-0">
+            <div class="col-lg-9 col-xl-6">
+                <h4 class="mb-3">{{ $pageTitle }}</h4>
+            </div>
+            <div class="col-lg-3 col-xl-6">
+                <ul class="list-inline mb-0 float-end">
+                    <li class="list-inline-item">
+                        <a href="{{ route('employees.exportExcel') }}" class="btn btn-outline-success">
+                            <i class="bi bi-download me-1"></i> to Excel
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="{{ route('employees.exportPdf') }}" class="btn btn-outline-danger">
+                            <i class="bi bi-download me-1"></i> to PDF
+                        </a>
+                    </li>
+                    <li class="list-inline-item">|</li>
+                    <li class="list-inline-item">
+                        <a href="{{ route('employees.create') }}" class="btn btn-primary">
+                            <i class="bi bi-plus-circle me-1"></i> Create Employee
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <hr>
+        <div class="table-responsive border p-3 rounded-3">
+            <table class="table table-bordered table-hover table-striped mb-0 bg-white datatable" id="employeeTable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>No</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($employees as $employee)
+                        <tr>
+                            <td>{{ $employee->firstname }}</td>
+                            <td>{{ $employee->lastname }}</td>
+                            <td>{{ $employee->email }}</td>
+                            <td>{{ $employee->age }}</td>
+                            <td>{{ $employee->position->name }}</td>
+                            <td>@include('employee.actions')</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+        </table>
+    </div>
+</div>
+
+    @vite('resources/js/app.js')
+@endsection
+</body>
+</html>
